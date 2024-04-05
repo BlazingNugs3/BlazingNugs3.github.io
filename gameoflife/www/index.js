2562ff45fee706798fe337d311145cdbc180e4a7
BlazingNugs3 <113144693+BlazingNugs3@users.noreply.github.com>
2024-04-04 17:55:40 -0700

    add framerate

--- gameoflife/www/index.js
+++ gameoflife/www/index.js
@@ -4,12 +4,22 @@ import { Universe } from "gameoflife";
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
+let fps = 0;
 
 const renderLoop = () => {
     pre.textContent = universe.render();
     universe.tick();
-  
-    requestAnimationFrame(renderLoop);
-  };
 
-  requestAnimationFrame(renderLoop);
+
+    const now = performance.now();
+    fps = 1000 / (now - renderLoop.lastRender);
+    renderLoop.lastRender = now;
+
+
+    fpsDisplay.textContent = `Frame Rate: ${Math.round(fps)} fps`;
+
+    requestAnimationFrame(renderLoop);
+};
+
+renderLoop.lastRender = performance.now();
+requestAnimationFrame(renderLoop);
